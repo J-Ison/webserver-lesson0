9068c4d01045d12d3422627c119235ad5a7a24b0
Jaramei <jcison22@gmail.com>
2021-01-22 19:41:12 -0600

    cloud function call

--- public/app.js
+++ public/app.js
@@ -1,6 +1,22 @@
+const f1 = firebase.functions().httpsCallable('doSomething')
+
 const buttonTag = document.getElementById('button-lucky')
 buttonTag.addEventListener('click', buttonClick)
 
-function buttonClick(){
+async function buttonClick(){
     console.log('button clicked')
+    try {
+        const n1 = Math.floor(Math.random() * 100);
+        const n2 = Math.floor(Math.random() * 100);
+        const result = await f1({n1, n2})
+        const ret = result.data
+        console.log(ret)
+        const message = document.getElementById('message')
+        message.innerHTML = 'n1 = ' + n1 + ', n2 = ' + n2 
+                + '<br>' + 'Add: ' + ret.add + ' Mul: ' + ret.mul
+    }
+    catch (e)
+    {
+       console.log(e); 
+    };
 }
